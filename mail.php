c483700a45418e0e062725fde0323fe87c771857
rakshakiitb <34677044+rakshakiitb@users.noreply.github.com>
2017-12-19 15:08:30 +0530

    Updating html and images file

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+$to = 'rakshak.uavs@gmail.com';
+$subject = 'New Email From Your web site:Rakshak';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$message = $message."from".$name."emailID".$email;
+$message=wordwrap($message,70);
+
+
+if($_POST){
+   mail($to, $subject, $message)
+   $feedback = 'your information has been successfully Send it';
+
+}
+?>
